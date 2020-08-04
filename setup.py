0a1367e49ea2a3688b21b454578c2953905ec9e4
Elian <elian@locallogic.co>
2020-08-04 17:00:24 -0400

    source code

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='obj2vec',
+    version='1.0',
+    packages=['models'],
+    url='https://github.com/eangius/obj2vec',
+    license='MIT',
+    author='Elian Angius',
+    description=
+        'General purpose object to vector machine learned embedding building module.',
+)
